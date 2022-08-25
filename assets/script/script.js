04063c412bd503432daa8385b2d8db45f930ed8f
MikeyRedmon <michaelredmond059@gmail.com>
2022-08-25 12:38:48 +0000

    add some quiz questions

---
+++ assets/script/script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        Question: "When was Ratchet and Clank for the Playstation 2 Released?",
+        A: "2001",
+        B: "2000",
+        C: "2002",
+        D: "1999",
+        Correct:"C",
+    },
+    {
+        Question: "In which galaxy does the original Ratchet and Clank take place?",
+        A: "Luna Galaxy",
+        B: "Solana Galaxy",
+        C: "Bogon Galaxy",
+        D: "Jatau Galaxy",
+        Correct: "B"
+    },
+    {
+        Question: "In Ratchet and Clank 2, who runs MegaCorp?",
+        A: "Mr Abercrombie Fizzwidget",
+        B: "Mr Jebidiah Appletop",
+        C: "Madam Delilah Crackbolt",
+        D: "Madam Katlin Penquil",
+        Correct:"A"
+    },
+    {
+        Question: "In Ratchet and Clank 2, where does the first area battle take place?",
+        A: "Nebula G34",
+        B: "Mnemonic Station",
+        C: "Xepher X-1",
+        D: "Maktar Resort",
+        Correct:"D"
+    },
+    {
+        Question: "At the start of Ratchet and Clank 3, who attacks Ratchets home?",
+        A: "Captain Qwark",
+        B: "The Tyhrranoids",
+        C: "Thugs-4-Less",
+        D: "A Unknown Space Phenomenon",
+        Correct:"B"
+    }
+]
